19a095287c787c87c4e6456032cf7d5b2a45bc29
LauraBonillaa <laurasofiabonilla@gmail.com>
2025-09-27 12:39:27 -0500

    Style: nav fixed

--- my-react-app/src/assets/screens/homepage/homepage.tsx
+++ my-react-app/src/assets/screens/homepage/homepage.tsx
@@ -2,11 +2,15 @@ import "./homepage.css";
 import Nav from "../../components/nav/nav";
 
 const HomePage = () => {
-  return <>
-    <Nav />
-    <h1>Home Page</h1>
-    
-  </>;
-}    
+  return (
+    <div className="homepage-container">
+      <Nav />
+      <div className="homepage-content">
+        <h1>Home Page</h1>
+        <p>Bienvenido a Loopi - Where connections loop</p>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;

df36a537e96cdafb03a2cfb919d676ad0fcf3a82
kikipou <kta2122006@gmail.com>
2025-09-27 18:42:40 -0500

    feat: hero image

--- my-react-app/src/assets/components/hero/hero.tsx
+++ my-react-app/src/assets/components/hero/hero.tsx
@@ -6,15 +6,6 @@ const Hero = () => {
       {/* Gradientes laterales */}
       <div className="hero-gradient-left"></div>
       <div className="hero-gradient-right"></div>
-      
-      {/* Imagen de fondo */}
-      <div className="hero-image-container">
-        {/* Aquí puedes reemplazar con tu imagen */}
-        <div className="hero-image-placeholder">
-          <div className="placeholder-text">Tu imagen aquí</div>
-        </div>
-      </div>
-      
       {/* Contenido superpuesto */}
       <div className="hero-content">
         <div className="hero-text">
@@ -26,6 +17,8 @@ const Hero = () => {
           </p>
         </div>
       </div>
+      {/* Imagen de fondo */}
+      <div className="hero-image-container"></div>
     </div>
   );
 };

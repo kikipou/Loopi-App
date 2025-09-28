bd692e28fb57e5b3b15ed3743dd1557f45baf2ad
kikipou <kta2122006@gmail.com>
2025-09-28 12:01:02 -0500

    feat: search page new

---
+++ my-react-app/src/assets/screens/searchpage/searchpage.tsx
@@ -0,0 +1,11 @@
+import './searchpage.css';
+
+const SearchPage = () => {
+  return <>
+    <div className="searchpage-container"> 
+      <h1 className="searchpage-title">Search</h1>
+    </div>
+  </>;
+}    
+
+export default SearchPage;

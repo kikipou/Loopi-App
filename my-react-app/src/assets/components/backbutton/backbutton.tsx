82037cf1536f121422679a939e94d6e1e76e45a9
kikipou <kta2122006@gmail.com>
2025-09-28 12:38:33 -0500

    fix: back button

--- my-react-app/src/assets/components/backbutton/backbutton.tsx
+++ my-react-app/src/assets/components/backbutton/backbutton.tsx
@@ -12,6 +12,7 @@ const BackButton = () => {
       className="back-button" 
       onClick={handleComeBack}
     >
+        <img className="back-icon" src="https://github.com/kikipou/Loopi-App/blob/cata/my-react-app/src/assets/imgs/back.png?raw=true" alt="back-button" />
     </button>
   );
 };
--- my-react-app/src/assets/screens/searchpage/searchpage.tsx
+++ my-react-app/src/assets/screens/searchpage/searchpage.tsx
@@ -4,9 +4,9 @@ import BackButton from '../../components/backbutton/backbutton';
 const SearchPage = () => {
   return <>
     <div className="searchpage-container"> 
-      <h1 className="searchpage-title">Search</h1>
-        <BackButton />
+      <h1 className="searchpage-title">What are you looping for</h1>
     </div>
+    <BackButton />
   </>;
 }    
 

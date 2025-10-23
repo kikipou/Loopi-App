d2e20aabde07fbe04de961ac0a87e6efc2d570a1
LauraBonillaa <laurasofiabonilla@gmail.com>
2025-10-23 17:37:30 -0500

    fix: new auth & database

--- my-react-app/src/components/nav2/nav2.tsx
+++ my-react-app/src/components/nav2/nav2.tsx
@@ -1,32 +1,30 @@
 import "./nav2.css";
 
 const Nav2 = () => {
-  const handleCategoryClick = (category: string) => {
-    setCategory(category);
-  };
-
-  // Función para capitalizar la primera letra
-  const capitalizeFirst = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  };
-
-  return (
-    <div className="nav2-container">
-      <div className="nav2-bar">
-        {categories.map((category) => (
-          <button
-            key={category}
-            className={`nav2-category ${
-              currentCategory === category ? "active" : ""
-            }`}
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category === "All" ? "All" : capitalizeFirst(category)}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+  // const handleCategoryClick = (category: string) => {
+  //   setCategory(category);
+  // };
+  // // Función para capitalizar la primera letra
+  // const capitalizeFirst = (str: string) => {
+  //   return str.charAt(0).toUpperCase() + str.slice(1);
+  // };
+  // return (
+  //   <div className="nav2-container">
+  //     <div className="nav2-bar">
+  //       {categories.map((category) => (
+  //         <button
+  //           key={category}
+  //           className={`nav2-category ${
+  //             currentCategory === category ? "active" : ""
+  //           }`}
+  //           onClick={() => handleCategoryClick(category)}
+  //         >
+  //           {category === "All" ? "All" : capitalizeFirst(category)}
+  //         </button>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Nav2;

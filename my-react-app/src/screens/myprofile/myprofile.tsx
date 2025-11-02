81c69619dc003e5559e7c2c607fa33d0aa711190
LauraBonillaa <laurasofiabonilla@gmail.com>
2025-11-02 16:19:42 -0500

    add: log out button in my profile

--- my-react-app/src/screens/myprofile/myprofile.tsx
+++ my-react-app/src/screens/myprofile/myprofile.tsx
@@ -1,11 +1,14 @@
-import './myprofile.css';
-
+import "./myprofile.css";
+import Button from "../../components/button/button";
 const MyProfile = () => {
-  return <>
-    <div className="myprofile-container"> 
-      <h1 className="myprofile-title">Your profile</h1>
-    </div>
-  </>;
-}    
+  return (
+    <>
+      <div className="myprofile-container">
+        <h1 className="myprofile-title">Your profile</h1>
+        <Button buttonplaceholder="Log out" buttonid="logout-button" />
+      </div>
+    </>
+  );
+};
 
 export default MyProfile;

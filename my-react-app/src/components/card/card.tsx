7d21e63082d9ccf954c445864291b2913ac056f8
kikipou <kta2122006@gmail.com>
2025-11-24 17:57:25 -0500

    ready for deploy

--- my-react-app/src/components/card/card.tsx
+++ my-react-app/src/components/card/card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 const Card = ({ post }: CardProps) => {
   return (
     <div className="card">
-      {post.image_url && <img src={post.image_url} alt={post.post_name} />}
+      {post.image_url && <img src={post.image_url} />}
       <h3>{post.post_name}</h3>
       <p className="post-description">{post.post_description}</p>
       <p className="post-professions"><strong>Profesiones:</strong> {post.post_professions}</p>
